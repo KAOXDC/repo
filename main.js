859fdbcdeec33ab0818b3ffa62b07f3c833d7e11
Diego Prado <kaoxdc@gmail.com>
2023-07-15 12:03:36 -0500

    html y javascript

---
+++ main.js
@@ -0,0 +1,36 @@
+// console.log("Hola!")
+
+// for (var  i = 0; i < 10; i=i+2){
+//     console.log(i)
+// }
+
+// op = true
+// while (op){
+//     console.log("Hola!")
+//     if (op){
+//         op = false
+//         // continue
+//     }else{
+//         break
+//     }
+// }
+
+// x = 10
+// do{
+//     x = x - 1
+//     console.log(x)
+// }while(x == 0)
+
+function saludar (n){
+    saludo = "Hola de nuevo!"
+    console.log(saludo + " " + n)
+    numeros = [1,2,"tres",4,5,6,8]
+    x = 1
+    y = 8
+    return numeros
+}
+
+nombre = prompt("ingrese el nombre : ")
+console.log(saludar(nombre))
+
+
